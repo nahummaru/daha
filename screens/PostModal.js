9dc7901118f0ed66e19b3b27f4cf1d3bb249c366
nahummaru <nahummaru1@gmail.com>
2022-12-24 02:55:34 -0600

    changes

---
+++ screens/PostModal.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+const PostModal = () => {
+  return (
+    <View>
+      <Text> fuck</Text>
+    </View>
+  )
+}
+
+export default PostModal
