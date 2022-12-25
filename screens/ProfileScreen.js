e756e3372322d0df758d5270041f9b20c6578d8a
Divya Vetticaden <divyav25@stanford.edu>
2022-12-24 21:27:15 -0800

    adding sign out functionality on the profile page

--- screens/ProfileScreen.js
+++ screens/ProfileScreen.js
@@ -1,14 +1,37 @@
 import { useNavigation } from '@react-navigation/native';
 import React, {useLayoutEffect} from 'react';
-import { Button, SafeAreaView,ScrollView, Text, View } from 'react-native'
+//import { auth } from '../config/firebase';
+import { getAuth, signOut } from "firebase/auth";
+import { AntDesign } from '@expo/vector-icons';
+import { Button, SafeAreaView,ScrollView, Text, View, TouchableOpacity } from 'react-native'
 import {useTailwind} from 'tailwind-rn';
 
 
+const auth = getAuth();
 
-const MessagesScreen = () => {
+const MessagesScreen = ({}) => {
     const tailwind = useTailwind();
     const navigation = useNavigation();
-
+    useLayoutEffect(() => {
+        navigation.setOptions({
+            headerRight: () => (
+                <TouchableOpacity style={{
+                    marginRight: 15
+                }}
+                    onPress={ signOutFunction }
+                >
+                    <AntDesign name="logout" size={24} color="white" />
+                </TouchableOpacity>
+            )
+        })
+    }, [])
+    const signOutFunction = () => {
+        signOut(auth).then(() => {
+            navigation.replace('LoginScreen')
+        }).catch((error) => {
+            //an error happened
+        });
+    }
     return (
         <ScrollView>
             <SafeAreaView>
