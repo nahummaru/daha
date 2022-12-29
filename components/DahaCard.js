39de2a8a9e98c8163a8f5290ce61d872e8260fae
nahummaru <nahummaru1@gmail.com>
2022-12-28 21:00:13 -0600

    add reading dahas

--- components/DahaCard.js
+++ components/DahaCard.js
@@ -2,14 +2,17 @@ import React from 'react'
 import { View, Text } from 'react-native'
 import { Container, Card, UserInfo, TopLevel, UserImg, UserName, UserInfoText, PostTime, PostImg, PostText, InteractionWrapper, InteractionText, Interaction } from '../styles/DahaStyles'
 import Ionicons from 'react-native-vector-icons/Ionicons';
+import { Timestamp, toDate } from 'firebase/firestore';
+
+// using the item.uidUser, we can then query for the profile picture
 
 const DahaCard = ({ item }) => {
+
   return (
     <Card>
-
       <TopLevel>
         <UserInfo>
-          <UserImg source={item.userImg} />
+          <UserImg source={require('../assets/users/user-7.jpg')} />
           <UserInfoText>
             <UserName>{item.userName}</UserName>
             <PostTime>{item.postTime}</PostTime>
--- screens/DahaScreen.js
+++ screens/DahaScreen.js
@@ -1,13 +1,17 @@
-import { Input } from '@rneui/themed'
-import React from 'react'
-import { View, Text, ScrollView, SafeAreaView, Button, FlatList } from 'react-native'
+import React, { useContext, useEffect, useLayoutEffect } from 'react'
+import { View, Text, ScrollView, SafeAreaView, SkeletonPlaceholder, FlatList } from 'react-native'
 import { useTailwind } from 'tailwind-rn'
 import { Container } from '../styles/DahaStyles'
 import Ionicons from 'react-native-vector-icons/Ionicons';
 import DahaCard from '../components/DahaCard'
+import { doc, onSnapshot, collection, getDocsFromServer, getDocs } from "firebase/firestore";
+import { AuthenticatedUserContext } from '../App.js';
+import { db } from '../config/firebase.js';
+import { set } from '@firebase/database';
+import useState from 'react-usestateref'
 
 
-const Posts = [
+const fakePosts = [
   {
     id: '1',
     userName: 'Nahum Maru',
@@ -60,20 +64,69 @@ const Posts = [
   },
 ];
 
+const dbRefDahas = collection(db, "dahas");
 
+
+
+
+// query for all the posts
 const DahaScreen = () => {
 
+  const { user, setUser } = useContext(AuthenticatedUserContext);
+  const [loading, setLoading] = useState(true);
+  const list = [];
+  const [posts, setPosts, postsReff] = useState([]);
+
+  useLayoutEffect(() => {
+    fetchPosts();
+    console.log('GOD PLZ HELP ME I WANNA KMS')
+  }, []);
+
+
+  const fetchPosts = async () => {
+    onSnapshot(dbRefDahas, docsSnap => {
+      docsSnap.forEach(doc => {
+        const { postText, postTime, uidUser } = doc.data();
+        list.push({
+          id: doc.id,
+          userName: uidUser,
+          userImg: require('../assets/users/user-7.jpg'),
+          postTime: '2 days ago',
+          post: postText,
+          bookmarked: false,
+        });
+      });
+
+      setPosts(list);
+      console.log('this is posts')
+      console.log(posts)
+      console.log(postsReff.current)
+      console.log(list)
+    });
+    
+    if (loading) {
+      setLoading(false);
+    }
+  }
+
   return (
-    <Container>
-      <FlatList
-        data={Posts}
-        renderItem={({ item }) => <DahaCard item={item} />}
-        keyExtractor={item => item.id}
-        showsHorizontalScrollIndicator ={false}
-        showsVerticalScrollIndicator={false}
-      />
-    </Container>
-  )
+    <SafeAreaView style={{ flex: 1 }}>
+      {loading ? (
+        <Text>hello</Text>
+      ) : (
+        <Container>
+          <FlatList
+            data={postsReff.current}
+            renderItem={({ item }) => console.log('hello what in the world') && console.log(posts) &&
+              <DahaCard item={item} />}
+            keyExtractor={item => item.id}
+            showsHorizontalScrollIndicator={false}
+            showsVerticalScrollIndicator={false}
+          />
+        </Container>
+      )}
+    </SafeAreaView>
+  );
 }
 
 export default DahaScreen
