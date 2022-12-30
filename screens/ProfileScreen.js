1bdd61425ff294d3b61edd74bc78c6e1ed3544f2
nahummaru <nahummaru1@gmail.com>
2022-12-30 16:57:28 -0600

    adding dynamic profiles

--- screens/ProfileScreen.js
+++ screens/ProfileScreen.js
@@ -1,6 +1,8 @@
 import { useNavigation } from '@react-navigation/native';
-import React, { useContext, useState } from 'react';
+import React, { useContext, useState, useEffect } from 'react';
 //import { auth } from '../config/firebase';
+import { db, auth, } from '../config/firebase';
+import { getDoc, doc, collection } from '@firebase/firestore';
 import { getAuth, signOut } from "firebase/auth";
 import { Entypo } from '@expo/vector-icons';
 import Stars from 'react-native-stars';
@@ -8,9 +10,6 @@ import { Button, SafeAreaView, ScrollView, Text, View, TouchableOpacity, StyleSh
 import { Avatar, Title, Caption, TouchableRipple } from 'react-native-paper';
 import { AuthenticatedUserContext } from '../App';
 import ProfileTopTabNavigator from '../navigator/ProfileTopTabNavigator';
-
-
-import useState from 'react-usestateref'
 import { Touchable } from 'react-native-web';
 import * as ImagePicker from 'expo-image-picker';
 
@@ -18,30 +17,39 @@ import * as ImagePicker from 'expo-image-picker';
 
 
 // we should pass in the user and populate the page with their specifc stuff
-
-const auth = getAuth();
-
 const ProfileScreen = () => {
     const navigation = useNavigation();
     const { user, setUser } = useContext(AuthenticatedUserContext);
-
-    // console.log(user)
-
+    const docRef = doc(db, "users", user.uid);
     const [image, setImage] = useState(null);
-
+    const [userInfo, setUserInfo] = useState(null)
+    
     const pickImage = async () => {
-      let result = await ImagePicker.launchImageLibraryAsync({
-        mediaTypes: ImagePicker.MediaTypeOptions.Images,
-        quality: 1,
-        allowsEditing: true
-      });
-  
-      if (!result.canceled) {
-        setImage(result.assets[0].uri);
-      }
-  
+        let result = await ImagePicker.launchImageLibraryAsync({
+            mediaTypes: ImagePicker.MediaTypeOptions.Images,
+            quality: 1,
+            allowsEditing: true
+        });
+
+        if (!result.canceled) {
+            setImage(result.assets[0].uri);
+        }
+
     };
 
+    useEffect(() => {
+        const fetchUserInfo = async () => {
+            try {
+                const docSnap = await getDoc(docRef);
+                const data = docSnap.data()
+                setUserInfo(data);
+            } catch (error) {
+                console.log('error', error)
+            }
+        }
+        fetchUserInfo();
+
+    }, []);
 
     return (
         <SafeAreaView style={styles.container}>
@@ -62,7 +70,7 @@ const ProfileScreen = () => {
                         <Title style={[styles.title, {
                             marginTop: 15,
                             marginBottom: 5,
-                        }]}>nahummaru</Title>
+                        }]}>{userInfo.username}</Title>
                         <Stars
                             default={2.5}
                             count={5}
@@ -70,16 +78,16 @@ const ProfileScreen = () => {
                             starSize={120}
 
 
-                            fullStar={<Entypo name={'star'} size ={30} style={[styles.myStarStyle]} />}
+                            fullStar={<Entypo name={'star'} size={30} style={[styles.myStarStyle]} />}
                             emptyStar={<Entypo name={'star'} size={30} style={[styles.myStarStyle, styles.myEmptyStarStyle]} />}
-                            halfStar={<Entypo name={'star'} size= {30} style={[styles.myStarStyle]} />}
+                            halfStar={<Entypo name={'star'} size={30} style={[styles.myStarStyle]} />}
                         />
                     </View>
                 </View>
             </View>
 
 
-            <ProfileTopTabNavigator>
+            <ProfileTopTabNavigator >
                 <Text>hello</Text>
             </ProfileTopTabNavigator>
 
