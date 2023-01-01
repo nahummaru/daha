7f377e3cac83baed3a40c46953b08daa4fb69bd0
Divya Vetticaden <divyav25@stanford.edu>
2022-12-31 22:14:58 -0800

    history card, history screen, dawa profile ui

--- screens/HistoryScreen.js
+++ screens/HistoryScreen.js
@@ -1,93 +1,127 @@
-import React, { useContext, useEffect, useLayoutEffect } from 'react'
-import { View, Text, ScrollView, SafeAreaView, SkeletonPlaceholder, StyleSheet, FlatList } from 'react-native'
-import Ionicons from 'react-native-vector-icons/Ionicons';
+import React, { useContext, useEffect } from 'react'
+import { View, Text, FlatList, StyleSheet, ScrollView, SafeAreaView } from 'react-native'
+import { AuthenticatedUserContext } from './../App';
+import HistoryCard from '../components/HistoryCard';
+import { db } from './../config/firebase';
 import useState from 'react-usestateref'
-import ReviewCard from '../components/ReviewCard';
-import { AntDesign, Entypo } from '@expo/vector-icons';
+import { doc, onSnapshot, collection, getDocsFromServer, query, getDocs, where } from "firebase/firestore";
 
 
 
-
-
-
-const Posts = [
-  {
-    id: '1',
-    userName: 'Carmel L',
-    userImg: require('../assets/users/user-3.jpg'),
-    rating: 1,
-    post:
-      'Nahum ruined my clothes',
-
-  },
-  {
-    id: '2',
-    userName: 'John Doe',
-    userImg: require('../assets/users/user-1.jpg'),
-    rating: 5,
-    post:
-      'Wore his basketball jersey to ksig and felt sexy.',
-  },
-  {
-    id: '3',
-    userName: 'Ken William',
-    userImg: require('../assets/users/user-4.jpg'),
-    rating: 4,
-    post:
-      'Reasonable Pricing',
-  },
-  {
-    id: '4',
-    userName: 'Selina Paul',
-    userImg: require('../assets/users/user-6.jpg'),
-    rating: 4,
-    post:
-      'Hey there, this is my test for a post of my social app in React Native.',
-  },
-  {
-    id: '5',
-    userName: 'Christy Alex',
-    userImg: require('../assets/users/user-7.jpg'),
-    rating: 4,
-    post:
-      'Hey there, this is my test for a post of my social app in React Native.',
-  },
-];
-
+// sets up db reference to 'dahas' collection
+const dbRefDahas = collection(db, "dahas");
+const dbRefUsers = collection(db, "users");
 
 const HistoryScreen = () => {
 
+  const { user, setUser } = useContext(AuthenticatedUserContext);
+  const [loading, setLoading] = useState(true);
+  const list = [];
+  const [posts, setPosts, postsReff] = useState([]);
+  // const [userInfo, setUserInfo, UserInfoReff] = useState(null);
+
+  // const [profilePic, setProfilePic] = useState(null);
+  // const [user, setProfilePic] = useState(null);
+
+  useEffect(() => {
+    // get info user info
+    fetchPosts();
+  }, []);
+
+  // ORDER POSTS BY MOST RECENT, ADD CATEGORIES TO DAHAS
+  const fetchUserInfo = async () => {
+    // create a object of userInfo to be accessed when fetching posts
+    const docsSnap = await getDocs(dbRefUsers);
+    const users = {};
+    docsSnap.forEach(doc => {
+      const { uidUser, username, timeCreated, profilePic } = doc.data();
+      users[uidUser] = {
+        username: username,
+        timeCreated: timeCreated,
+      }
+      //console.log(users)
+
+    })
+    return users
+  }
+
+  const fetchPosts = async () => {
+    const userInfo = await fetchUserInfo();
+
+    const q = query(dbRefDahas, where("uidUser", "==", user.uid)); // orderBy("field2", "asc")
+
+    onSnapshot(q, (docsSnap) => {
+      docsSnap.forEach(doc => {
+        const { postText, postTime, uidUser } = doc.data();
+
+        list.push({
+          //  FIX THIS: IT IS NOT A GOOD LONG TERM FIX -- WHY IS THERE DUPLICATE DOC.IDs?. THIS COULD MEAN DUPLICATE POSTS BEING RENDER -- HOWEVER COULD ALSO JUST A WARNING WE CAN IGNORE
+          // id; doc.id,
+          id: list.length,
+          userName: userInfo[uidUser].username,
+          postTime: postTime.toDate(),
+          post: postText,
+          bookmarked: true,
+        });
+      });
+      // sorts posts with newest on the top
+      let sortedPosts = list.sort(
+        (p1, p2) => (p1.postTime.getTime() < p2.postTime.getTime()) ? 1 : (p1.postTime.getTime() > p2.postTime.getTime()) ? -1 : 0);
+      //console.log('yooooo')
+      //console.log(sortedPosts)
+      setPosts(sortedPosts);
+    });
+
+    if (loading) {
+      setLoading(false);
+    }
+  }
+
   return (
     <View style={{ flex: 1, backgroundColor: '#fff', padding: 15, borderRadius: 5 }}>
-       <SafeAreaView style={{flex: 1}}>
-      <ScrollView style={styles.ScrollView}>
-      <Text style={styles.title}> Active </Text>
-      <SafeAreaView>
-    <FlatList
-      data={Posts}
-      renderItem={({ item }) => <ReviewCard item={item} />}
-      keyExtractor={item => item.id}
-      showsHorizontalScrollIndicator={false}
-      showsVerticalScrollIndicator={false}
-    />
-    </SafeAreaView>
-    <Text style={styles.title}>Completed</Text>
+      <ScrollView>
+      <Text style={styles.title}>Active</Text>
+      <SafeAreaView style={{flex: 1}}>
+      <FlatList
+        data={postsReff.current}
+        renderItem={({ item }) => {
+          return (<HistoryCard item={item} />)
+        }}
+        keyExtractor={item => item.id}
+        showsHorizontalScrollIndicator={false}
+        showsVerticalScrollIndicator={false}
+      />
+      </SafeAreaView>
+      <Text style={styles.title}>Completed</Text>
+      <FlatList
+        data={postsReff.current}
+        renderItem={({ item }) => {
+          return (<HistoryCard item={item} />)
+        }}
+        keyExtractor={item => item.id}
+        showsHorizontalScrollIndicator={false}
+        showsVerticalScrollIndicator={false}
+      />
     </ScrollView>
-    </SafeAreaView>
-  </View>
+    </View>
   )
 }
+const styles = StyleSheet.create({
+  container: {
+    marginTop: 20,
+    flex: 1,
+    flexDirection: 'column',
+    position: 'relative ',
+  },
+  title: {
+    fontSize: 35,
+    fontWeight: 'bold',
+    color: "#a5353a",
+    alignSelf: "left",
+    paddingBottom: 24,
+    marginTop: '5%'
 
+  }
+})
 
 export default HistoryScreen
-
-const styles = StyleSheet.create({
-  title: {
-    fontSize: 30,
-    fontWeight: 'bold',
-    marginLeft: '1%',
-    marginBottom: '5%',
-    marginTop: '5%'
-  }
- 
-});
