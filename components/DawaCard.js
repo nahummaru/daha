82e15a363ad1beb93900254798eb2bcdf9b62cd9
nahummaru <nahummaru1@gmail.com>
2023-01-01 03:32:48 -0600

    more refactor of dawacard

--- components/DawaCard.js
+++ components/DawaCard.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { View, Text , Image, TouchableOpacity} from 'react-native'
+import { View, Text, Image, TouchableOpacity } from 'react-native'
 import { Container, Card, UserInfo, TopLevel, UserImg, UserName, UserInfoText, PostDist, PostImg, PostText, InteractionWrapper, InteractionText, Interaction, StateText, LocationWrapper } from '../styles/DawaStyles'
 import Ionicons from 'react-native-vector-icons/Ionicons';
 
@@ -11,26 +11,20 @@ const DawaCard = ({ item }) => {
 
   // code some logic to get the prices and shi
 
+
+  // define a function that when the image is clicked, a screen will popup with more info
+
+
+
   return (
-    <TouchableOpacity style={{ flex: 1, alignItems: 'center', backgroundColor: '#f8f8f8', padding: 20,borderRadius: '10%', marginTop: 2.5, marginBottom: 2.5, marginLeft: 5, marginRight: 5 }}>
+    <View style={{ flex: 1, alignItems: 'center', backgroundColor: '#f8f8f8', padding: 20, borderRadius: '10%', marginTop: 2.5, marginBottom: 2.5, marginLeft: 5, marginRight: 5 }}>
       <Image style={{ width: '100%', height: 250, marginTop: 5, borderRadius: '10%', marginBottom: '5%' }} source={item.postImg} />
+      
       <View style={{ flexDirection: 'row', alignSelf: 'flex-start' }}>
-        <View
-          style={{
-            flexDirection: "row",
-          }}>
-          <Text style={{size: 12, fontWeight: 'bold', color: '#b22222' }}>{item.StateText}</Text>
-        </View>
 
-        <View
-          style={{
-            flexDirection: "row",
-            alignItems: 'flex-end'
-          }}>
+          <Text style={{ size: 12, fontWeight: 'bold', color: '#b22222' }}>{item.StateText}</Text>
+          <Ionicons style={{  }} name={starIcon} size={17} />
 
-          <Ionicons style={{ marginLeft: '10%', marginTop: '-10%' }} name={starIcon} size={17} />
-
-        </View>
       </View>
 
       <Text style={{ size: 14, fontWeight: 'bold', marginBottom: 15, alignSelf: 'flex-start' }}>{item.post}</Text>
@@ -39,7 +33,7 @@ const DawaCard = ({ item }) => {
         <Ionicons name="location-outline" size={25} />
         <Text style={{ size: 12, }}>{item.postDist}</Text>
       </View>
-    </TouchableOpacity>
+    </View>
   )
 }
 
